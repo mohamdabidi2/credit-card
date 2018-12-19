f1b552b9c0566b4c6be5efa20edaf2ddbb73ab5e
mohamdabidi2 <45004605+mohamdabidi2@users.noreply.github.com>
2018-12-19 14:11:24 +0100

    Updates

---
+++ precache-manifest.43b9130744010b995ab56d661a4d0fe9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "007dcb603e915afd331ec2dc01661f94",
+    "url": "/credit-card/static/media/carte.007dcb60.PNG"
+  },
+  {
+    "revision": "a8c8030be44fdafe49518bd5325953a4",
+    "url": "/credit-card/static/media/caer.a8c8030b.png"
+  },
+  {
+    "revision": "55686cba959a91c09178ae0768068a4d",
+    "url": "/credit-card/static/media/background.55686cba.jpg"
+  },
+  {
+    "revision": "c447cfde0f5229f80974",
+    "url": "/credit-card/static/js/runtime~main.c447cfde.js"
+  },
+  {
+    "revision": "ac7a38b2b3ec91d7757c",
+    "url": "/credit-card/static/js/main.ac7a38b2.chunk.js"
+  },
+  {
+    "revision": "25aee797ba5d66677279",
+    "url": "/credit-card/static/js/1.25aee797.chunk.js"
+  },
+  {
+    "revision": "ac7a38b2b3ec91d7757c",
+    "url": "/credit-card/static/css/main.461e7b90.chunk.css"
+  },
+  {
+    "revision": "12c0511fc741a9ac0ffae0933e48c743",
+    "url": "/credit-card/index.html"
+  }
+];
